96e0b7666693ab0d34e5cc93cac25bf61c749c3a
tatenami <ttnm139th@gmail.com>
2024-08-06 22:00:47 +0900

    create basic item box UI and Scripts

---
+++ Assets/Scripts/Akiyama/A_PCPassWordManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class A_PCPassWordManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI inPWText;
+    private int pwColumn = 0;
+    private string[] pwArray = new string[7];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PressKeyButton(string inKey)
+    {
+        if (inKey == "back")
+        {
+            PWClear();
+        }
+        else if (inKey == "enter")
+        {
+            if (string.Join("", pwArray) == "TANUKI")
+            {
+                Debug.Log("Clear!");
+            }
+            else
+            {
+                Debug.Log("Wrong!");
+            }
+        }
+        else
+        {
+             if (pwColumn < 6)
+            {
+                pwArray[pwColumn] = inKey;
+                inPWText.text = string.Join("", pwArray);
+                pwColumn++;
+            }
+            else if (pwColumn == 6)
+            {
+                PWClear();
+            } 
+        }
+    }
+
+    private void PWClear()
+    {
+        for (int i = 0; i < pwArray.Length; i++)
+        {
+            pwArray[i] = "";
+        }
+            inPWText.text = string.Join("", pwArray);  
+            pwColumn = 0;      
+    }
+}
--- Assets/Scripts/Common/ItemUIManager.cs
+++ Assets/Scripts/Common/ItemUIManager.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemData : MonoBehaviour
+public class ItemUIManager : MonoBehaviour
 {
+    [SerializeField] GameObject ItemBoxPanel;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,12 @@ public class ItemData : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OpenItemBox() {
+      ItemBoxPanel.SetActive(true);
+    }
+    public void CloseItemBox() {
+      ItemBoxPanel.SetActive(false);
     }
 }
