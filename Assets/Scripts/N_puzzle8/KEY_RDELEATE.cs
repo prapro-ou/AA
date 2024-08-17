8876f11dae77fda983e181f28b31aa50de328049
nasu24710 <110810258+nasu24710@users.noreply.github.com>
2024-08-17 11:37:28 +0900

    Update AA

---
+++ Assets/Scripts/N_puzzle8/KEY_RDELEATE.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KEY_RDELEATE : MonoBehaviour
+{
+   public GameObject KEY_R;
+
+   public void Click()
+   {
+    KEY_R.SetActive(false);
+   }
+
+}
