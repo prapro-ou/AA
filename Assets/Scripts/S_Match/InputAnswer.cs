a87d677b5cbf5de7807f26b7a12c16b34ac19bc9
Sumaayuto <166295061+Sumaayuto@users.noreply.github.com>
2024-08-21 21:01:00 +0900

    8/21

--- Assets/Scripts/S_Match/InputAnswer.cs
+++ Assets/Scripts/S_Match/InputAnswer.cs
@@ -26,6 +26,7 @@ public class InputAnswer : MonoBehaviour
             //Debug.Log("クリア！！");
             memo.SetActive(true);
             memo_image.SetActive(true);
+            GetComponent<AudioSource>().Play();
 
         }
         else
